ddc489f2c73a9769f7338fb8158579f7b29a22d6
Marcelo <marcelovqvd@gmail.com>
2022-08-18 09:26:30 -0300

    Consolidar condição

--- src/calcRefatorada.ts
+++ src/calcRefatorada.ts
@@ -23,35 +23,25 @@ function isValidDate(date) {
 	return date != null && date != undefined && date instanceof Date && date.toString() !== "Invalid Date";
 }
 
-export function calculateRide (segments) {
+export function calculateRide(segments) {
 	let fare = 0;
 	for (const segment of segments) {
-		if (isValidyDistance(segment.distance)) {
-			if (isValidDate(segment.date)) {
-				if (isOvernight(segment.date)) {			
-					const notsunday = segment.date.getDay(); 
-					if (!isSunday(segment.date)) {						
-						fare += segment.distance * OVERNIGHT_FARE;
-					} else {
-						fare += segment.distance * OVERNIGHT_SUNDAY_FARE;
-						}
-				} else {
-					if (!isSunday(segment.date)) {
-						fare += segment.distance * SUNDAY_FARE;
-					} else {
-						fare += segment.distance * NORMAL_FARE;
-  				}
-				}
-			} else {
-				throw new Error("Invalid Date")
-			}
-		} else {
-			throw new Error("Invalid Distance")
-		}		
+		if (!isValidyDistance(segment.distance)) throw new Error("Invalid Distance");
+		if (!isValidDate(segment.date)) throw new Error("Invalid Date");
+		if (isOvernight(segment.date) && !isSunday(segment.date)) {
+				fare += segment.distance * OVERNIGHT_FARE;
+				continue;
+		}
+		if (isOvernight(segment.date) && isSunday(segment.date)) {
+			fare += segment.distance * OVERNIGHT_SUNDAY_FARE;
+			continue;
+		}
+		if (isSunday(segment.date)) {
+			fare += segment.distance * SUNDAY_FARE;
+			continue;
+		} 
+		fare += segment.distance * NORMAL_FARE;
 	}
-	if (fare < MIN_FARE) {
-		return MIN_FARE;
-	} else {
-		return fare;
-	}
+
+	return fare < MIN_FARE ? MIN_FARE : fare;
 }
