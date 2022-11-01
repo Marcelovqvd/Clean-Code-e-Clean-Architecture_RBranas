3bc37d0df8a9348d4bcad7af5c9f9296e4e46afc
Marcelo <marcelovqvd@gmail.com>
2022-10-31 23:27:34 -0300

    feat: adjust

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-
-export function Calc(movArray: any) {
-    const fare = movArray[0].dist;
-    return fare;
-}
-
-const rides = [{dist: 10, day: 2}, {dist: 5, day: 2}]
-Calc(rides)
--- test/calc.test.ts
+++
@@ -1,6 +0,0 @@
-import { Calc } from "../src"
-
-test("Should calculate the value of a normal ride", () => {
-    const fare = Calc([{dist: 10, day: 2}, {dist: 5, day: 2}])
-    expect(fare).toBe(10)
-})
